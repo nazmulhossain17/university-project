2ab55466e959e36df452a76854c380b24005599e
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-20 00:09:09 +0600

    academicSemesterTitleCodeMapper

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,14 @@
+import ApiError from '../../../errors/ApiErrors';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import AcademicSemester from './academicSemester.model';
 
 const createSemester = async (
   payload: IAcademicSemester,
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(400, 'Invalid Semester Code');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
