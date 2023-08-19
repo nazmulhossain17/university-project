eda8c7addd23782edf06056a3f05ffefffa6ccbd
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-08-19 11:59:11 +0600

    handleZodError

--- src/errors/handleZodError.ts
+++ src/errors/handleZodError.ts
@@ -1,11 +1,21 @@
-import { ZodError } from 'zod';
+import { ZodError, ZodIssue } from 'zod';
+import { IGenericErrorResponse } from '../interfaces/common';
+import { IGenericErrorMessage } from '../interfaces/error';
 
-const handleZodError = (error: ZodError) => {
+const handleZodError = (error: ZodError): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = error.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue?.message,
+    };
+  });
+
   const statusCode = 400;
+
   return {
     statusCode,
     message: 'Validation Error',
-    errorMessages: error,
+    errorMessages: errors,
   };
 };
 
